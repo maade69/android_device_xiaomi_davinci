6d41e3541f84bf7d45b5d24de01bf877c4561a43
arter97 <qkrwngud825@gmail.com>
2020-10-21 17:34:04 +0300

    davinci: init: workaround safetynet check
    Change from original workaround: don't spoof ro.boot.verifiedbootstate, as it is overriden already
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -31,6 +31,8 @@
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 using android::base::GetProperty;
@@ -71,6 +73,48 @@ void set_ro_product_prop(const std::string &prop, const std::string &value) {
         property_override(prop_name.c_str(), value.c_str(), false);
     }
 };
+
+	/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+    "ro.boot.vbmeta.device_state",
+    "ro.boot.flash.locked",
+    "ro.boot.selinux",
+    "ro.boot.veritymode",
+    "ro.boot.warranty_bit",
+    "ro.warranty_bit",
+    "ro.debuggable",
+    "ro.secure",
+    "ro.build.type",
+    "ro.build.tags",
+    "ro.build.selinux",
+    NULL
+};
+
+ static const char *snet_prop_value[] = {
+    "locked",
+    "1",
+    "enforcing",
+    "enforcing",
+    "0",
+    "0",
+    "0",
+    "1",
+    "user",
+    "release-keys",
+    "1",
+    NULL
+};
+
+ static void workaround_snet_properties() {
+
+     // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
+
+     chmod("/sys/fs/selinux/enforce", 0640);
+    chmod("/sys/fs/selinux/policy", 0440);
+}
 
 void vendor_load_properties() {
     std::string region;
@@ -117,4 +161,7 @@ void vendor_load_properties() {
     }
 
     property_override("ro.boot.hardware.revision", hardware_revision.c_str());
+    
+    // Workaround SafetyNet
+    workaround_snet_properties();
 }
