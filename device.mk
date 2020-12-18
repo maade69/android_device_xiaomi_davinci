15cfe05aecd0f833e71a09e84ca64c62f0506a80
maade69 <p1nchp0d@bk.ru>
2020-12-18 16:59:42 +0300

    davinci: remove unused strings

--- device.mk
+++ device.mk
@@ -154,7 +154,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # FM
 PRODUCT_PACKAGES += \
