daf3bd2888d0f4175ea965ead276a773cc47a407
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-02-03 12:35:19 +0000

    davinci: init: set feb sunfish fp
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>
    Signed-off-by: maade <p1nchp0d@bk.ru>

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -149,8 +149,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys";
-    description = "redfin-user 11 RQ1A.201205.010 6953398 release-keys";
+    fingerprint = "google/sunfish/sunfish:11/RQ1A.210205.004/7038034:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
