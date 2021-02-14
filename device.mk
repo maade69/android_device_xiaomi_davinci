202b499766b0bb1b92d18b9c4ce2dd82686f5b79
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-02-14 14:30:48 +0000

    davinci: switch back to libperfmgr power hal
    This reverts commit 17169cd16ce34bf6723a2d4ae482f6022352c150.
    
    Signed-off-by: maade <p1nchp0d@bk.ru>

--- device.mk
+++ device.mk
@@ -303,7 +303,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power@1.3-service.sm6150-libperfmgr \
+    powerhint.json
 
 # Prebuilts
 PRODUCT_PACKAGES += \
@@ -356,7 +357,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+    hardware/google/pixel
 
 # Telephony
 PRODUCT_PACKAGES += \
